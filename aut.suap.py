414b99556f834b7c6a3db3bc7904352cc0a07ac9
lalvirt <lucianoandrade@oi.com.br>
2017-07-12 20:39:12 -0300

    Consulta Localização suap

---
+++ aut.suap.py
@@ -0,0 +1,14 @@
+import json
+import requests
+from getpass import getpass
+
+a=input('Entre com a matrícula:  \n')
+
+def Consulta_suap(matricula):
+	r = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/', auth=(input('\nMatricula: '),getpass('\nSenha: ')))
+	print(r.text)
+	print(r.url)
+
+if __name__ == '__main__':
+	Consulta_suap(a)
+
