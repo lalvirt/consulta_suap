e74538aa49b128d8c8fe881e4e02ae5be7eaa317
Luciano Andrade <lucianoandrade@oi.com.br>
2017-07-19 20:05:16 -0300

    correção da chamada a matrícula

--- aut.suap.py
+++ aut.suap.py
@@ -5,10 +5,11 @@ from getpass import getpass
 a=input('Entre com a matrícula:  \n')
 
 def Consulta_suap(matricula):
-	r = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/', auth=(input('\nMatricula: '),getpass('\nSenha: ')))
+	r = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/', auth=(a,getpass('\nSenha: ')))
 	print(r.text)
 	print(r.url)
+	return matricula
+
 
 if __name__ == '__main__':
 	Consulta_suap(a)
-
